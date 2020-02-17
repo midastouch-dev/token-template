dd205065a92cf7b288da9381dd711073adf90940
tylerfyu <tylerfyu@outlook.com>
2020-02-17 14:03:14 -0300

    Save deployed address to file

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,13 @@
 // This script uses @nomiclabs/buidler-truffle5
 const ERC20Factory = artifacts.require("ERC20Factory");
+const fs = require('fs')
 
 async function main() {
   const erc20Factory = await ERC20Factory.new();
 
-  console.log("ERC20Factory address:", erc20Factory.address);
+  fs.writeFileSync('addresses.json', JSON.stringify({
+    erc20Factory: erc20Factory.address
+  }))
 }
 
 main()
